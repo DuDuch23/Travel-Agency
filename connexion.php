e875cbaadbb2fef4d57f2f3d8fb83ba105c5d07f
DuDuch23 <alexduduch77@gmail.com>
2024-05-20 14:54:39 +0200

    burger code

---
+++ connexion.php
@@ -0,0 +1,13 @@
+<?php
+try{
+    $database = new PDO('mysql:host=localhost;dbname=store','root','');
+    $database->setAttribute(PDO::ATTR_ERRMODE, pdo::ERRMODE_EXCEPTION);
+}catch(Exception $e){
+    die('ERROR: ' . $e->getMessage());
+}
+
+$results = $database->query('SELECT first_name FROM customers');
+
+// while($row = $results->fetch()){
+//     echo $row['first_name'].'<br>';
+// }
